7e43880ef058c6e5f8905f43254d86a3d72c49d1
Eikra <ikramecharaki@gmail.com>
2025-05-31 22:34:51 +0100

    tests outide container

--- nestjs-app/script.sh
+++ nestjs-app/script.sh
@@ -7,18 +7,10 @@ docker rm $(docker ps -aq)     # Remove all containers, including stopped ones
 # Remove stopped containers
 docker container prune -f
 
-# rm -rf /home/iecharak/data/mariadb/*
-# rm -rf /home/iecharak/data/wordpress/*
 
 
 # Remove unused images (both dangling and tagged)
 # docker image prune -af
-
-# docker rmi mariadb:42  nginx:42  wordpress:42 mariadb:latest  nginx:latest  wordpress:latest
-
-# Clean up Docker cache (if needed)
-# # Be cautious with this step
-# rm -rf /var/lib/docker/*
 
 # Remove builder cache
 # docker builder prune -f
